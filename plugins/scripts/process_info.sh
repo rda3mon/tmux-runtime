48fb768241159a039201187ee9f1e5fbd8dad7e9
Mallikarjun <mallikvarjun@gmail.com>
2019-02-10 09:19:19 +0530

    fix split ssh window header

--- plugins/scripts/process_info.sh
+++ plugins/scripts/process_info.sh
@@ -13,22 +13,26 @@ current_pid=$$
 
 pid="${pid[@]/$current_pid}"
 
+full_command=$(ps --no-headers --ppid $ppid -o command)
+pid_command=$(ps --no-headers --ppid $ppid -o pid)
+cpu_command=$(ps --no-headers --ppid $ppid -o %cpu)
+memory_command=$(ps --no-headers --ppid $ppid -o %mem)
 if [ -z $pid ]; then
-	echo ""
+	full_command=$(ps --no-headers --pid $ppid -o command)
+	pid_command=$(ps --no-headers --pid $ppid -o pid)
+	cpu_command=$(ps --no-headers --pid $ppid -o %cpu)
+	memory_command=$(ps --no-headers --pid $ppid -o %mem)
+fi
+
+if [ $cmd == 'ssh' ]; then
+	ssh_host=$(echo "$full_command" | grep -o '[\.a-zA-Z0-9-]*$')
+	ssh_user=$(ssh -G $ssh_host | grep '^user ' | sed 's/user //')
+	ssh_port=$(ssh -G $ssh_host | grep '^port ' | sed 's/port //')
+
+	remote_mem=$(ssh -l $ssh_user -p $ssh_port $ssh_host "bash -s" < /home/mallikarjunv/.tmux/plugins/scripts/mem_percentage.sh)
+	remote_load=$(ssh -l $ssh_user -p $ssh_port $ssh_host "bash -s" < /home/mallikarjunv/.tmux/plugins/scripts/load_info.sh)
+
+	echo "| CMD:$full_command R:[ $remote_mem | $remote_load ] L:[ PID:$pid_command | CPU:$cpu_command | Mem:$memory_command ] "
 else
-	full_command=$(ps --no-headers --ppid $ppid -o command)
-
-	if [ $cmd == 'ssh' ]; then
-		ssh_host=$(echo "$full_command" | grep -o '[\.a-zA-Z0-9-]*$')
-		ssh_user=$(ssh -G $ssh_host | grep '^user ' | sed 's/user //')
-		ssh_port=$(ssh -G $ssh_host | grep '^port ' | sed 's/port //')
-
-		remote_mem=$(ssh -l $ssh_user -p $ssh_port $ssh_host "bash -s" < /home/mallikarjunv/.tmux/plugins/scripts/mem_percentage.sh)
-		remote_load=$(ssh -l $ssh_user -p $ssh_port $ssh_host "bash -s" < /home/mallikarjunv/.tmux/plugins/scripts/load_info.sh)
-
-		echo "| CMD:$(ps --no-headers --ppid $ppid -o command) R:[ $remote_mem | $remote_load ] L:[ PID:$(ps --no-headers --ppid $ppid -o pid) | CPU:$(ps --no-headers --ppid $ppid -o %cpu) | Mem:$(ps --no-headers --ppid $ppid -o %mem) ] "
-	else
-		echo "| CMD:$(ps --no-headers --ppid $ppid -o command) L:[ PID:$(ps --no-headers --ppid $ppid -o pid) | CPU:$(ps --no-headers --ppid $ppid -o %cpu) | Mem:$(ps --no-headers --ppid $ppid -o %mem) ]"
-	fi
-
+	echo "| CMD:$full_command L:[ PID:$pid_command | CPU:$cpu_command | Mem:$memory_command ]"
 fi
